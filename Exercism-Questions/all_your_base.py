bb4db855bba8c3ce31b1a8058ffa1f2e0aea21c6
Yoojin93 <yoojinbae93@gmail.com>
2023-05-02 20:10:34 -0500

    all your base

---
+++ Exercism-Questions/all_your_base.py
@@ -0,0 +1,3 @@
+"""Convert a number, represented as a sequence of digits in one base, to any other base.
+
+Implement general base conversion. Given a number in base a, represented as a sequence of digits, convert it to base b."""
