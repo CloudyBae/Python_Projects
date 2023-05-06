9e29732e38e6ff0855a59dba4892455f95bdbcf2
Yoojin93 <yoojinbae93@gmail.com>
2023-05-06 17:13:38 -0500

    acronym

--- Exercism-Questions/acronym.py
+++ Exercism-Questions/acronym.py
@@ -1,3 +1,11 @@
+"""Convert a phrase to its acronym.
+
+Techies love their TLA (Three Letter Acronyms)!
+
+Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
+
+Punctuation is handled as follows: hyphens are word separators (like whitespace); all other punctuation can be removed from the input."""
+
 def abbreviate(words):
     new_word = words
     for letters in words:
